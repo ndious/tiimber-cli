31d0706376959817ffb847b7c625205ff9ba9f78
Dufreche <n.dufreche@gmail.com>
2016-03-22 20:58:56 +0000

    Updating PathResolver

--- src/Tiimber/Cli/PathResolver.php
+++ src/Tiimber/Cli/PathResolver.php
@@ -6,6 +6,10 @@ class PathResolver
 {
   public function resolve($dir)
   {
+    if (strpos($dir, 'vendor/ndufreche/tiimber-cli')) {
+      $dir = dirname(dirname(dirname($dir)));
+    }
+
     return $dir;
   }
 }
